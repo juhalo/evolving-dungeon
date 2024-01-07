9375f29d1d3646f9a22d3762a92bcf7730b9d5a6
juhalo <107361221+juhalo@users.noreply.github.com>
2024-01-07 15:04:50 +0200

    Create test for loading fonts

--- tests/DungeonGenericTests.cpp
+++ tests/DungeonGenericTests.cpp
@@ -1,8 +1,11 @@
-#include <gtest/gtest.h>
+#include "Textures.hpp"
 #include <SFML/Graphics.hpp>
+#include <gtest/gtest.h>
+#include <iostream>
 
 // Demonstrate some basic assertions, checks that googletest load correctly.
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest, BasicAssertions)
+{
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
@@ -10,7 +13,8 @@ TEST(HelloTest, BasicAssertions) {
 }
 
 // Check that SFML loads correctly
-TEST(HelloTest, SFMLLoading) {
+TEST(HelloTest, SFMLLoading)
+{
     sf::Vector2i vectorOne = sf::Vector2i(0, -1);
     sf::Vector2i vectorTwo = sf::Vector2i(0, -1);
     EXPECT_EQ(vectorOne.x, vectorTwo.x);
@@ -19,4 +23,13 @@ TEST(HelloTest, SFMLLoading) {
     sf::Vector2i vectorThree = sf::Vector2i(0, 1);
     EXPECT_EQ(vectorOne.x, vectorThree.x);
     EXPECT_NE(vectorOne.y, vectorThree.y);
+}
+
+// Check that static file loading works correctly
+TEST(HelloTest, StaticFileLoading)
+{
+    bool texturesLoadedSuccessfully = ED::Texture::loadTextures();
+    // Needs to be assert for the next part to not cause errors and break the test
+    ASSERT_EQ(true, texturesLoadedSuccessfully);
+    EXPECT_EQ(ED::Texture::font->getInfo().family, "Press Start 2P");
 }
