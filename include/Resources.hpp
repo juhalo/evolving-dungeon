f597a1cd7d1297082dc2076ad711e6424b79a422
juhalo <107361221+juhalo@users.noreply.github.com>
2024-01-09 00:27:56 +0200

    Create header for Resources template class

---
+++ include/Resources.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+
+template <typename ResourceType, typename ID>
+class Resources {
+public:
+    /**
+     * @brief Load specified resource
+     *
+     * @param id Identifier of the resource
+     * @param filename File name of the resource
+     */
+    void load(ID id, const std::string& filename);
+
+    /**
+     * @brief Get resource object
+     *
+     * @param id Identifier of the resource
+     * @return The resource object
+     */
+    Resource& getResource(ID id);
+
+    /**
+     * @brief Get constant reference to the resource object
+     *
+     * @param id Identifier of the resource
+     * @return Constant reference to the resource object
+     */
+    const Resource& getResource(ID id) const;
+
+private:
+    /**
+     * @brief Map of identifier to resource type
+     *
+     */
+    std::map<ID, std::unique_ptr<ResourceType>> m_resources;
+};
